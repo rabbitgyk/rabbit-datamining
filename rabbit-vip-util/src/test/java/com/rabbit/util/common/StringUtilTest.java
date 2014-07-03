c650e73f9c851022f0d2697f019b93a3b1de921a
= <=>
2014-07-03 23:18:48 +0800

    test add

---
+++ rabbit-vip-util/src/test/java/com/rabbit/util/common/StringUtilTest.java
@@ -0,0 +1,14 @@
+package com.rabbit.util.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
