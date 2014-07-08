cbf9f8889a16d6b44074fd03f7abcac7766f9132
guoyankui <yankuiguo@gmail.com>
2014-07-08 13:43:22 +0800

    controller

--- rabbit-data-web/src/main/java/com/rabbit/data/controller/TestController.java
+++ rabbit-data-web/src/main/java/com/rabbit/data/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.rabbit.data.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 	
 	@RequestMapping("/test")
-	public String index(){
+	public String index(HttpServletRequest request){
+		request.setAttribute("username", "rabbit-guoyankui");
 		return "/test/test";
 	}
 
