d8fd4195b7c296785f64410c7e18e4af56a9cff0
guoyankui <yankuiguo@gmail.com>
2014-09-07 22:57:12 +0800

    solved the candy problem

---
+++ rabbit-thinkInJava/src/main/java/com/rabbit/think/leetcode/Candy.java
@@ -0,0 +1,51 @@
+package com.rabbit.think.leetcode;
+
+/**
+ * There are N children standing in a line. Each child is assigned a rating value.
+ * You are giving candies to these children subjected to the following requirements:
+ * Each child must have at least one candy.
+ * Children with a higher rating get more candies than their neighbors.
+ * What is the minimum candies you must give?
+ * 
+ * @author rabbit
+ * @date   Sep 7, 2014
+ */
+public class Candy {
+
+	public static void main(String[] args) {
+		int[] ratings = {12,9,10,5,6};
+		System.out.println(candy(ratings));
+	}
+	
+	/**
+	 * 将所有孩子遍历两遍，先从左到右，再从右到左
+	 * @param ratings
+	 * @return
+	 */
+	public static int candy(int[] ratings) {
+		int n = ratings.length;
+		int[] candys = new int[n];
+		//每个孩子与其左侧的比较，满足条件
+		candys[0] = 1;
+		for(int i=1; i<n; i++){
+			if(ratings[i] > ratings[i-1]){
+				candys[i] = candys[i-1] + 1;
+			}else{
+				candys[i] = 1;
+			}
+		}
+		//每个孩子与其右侧的比较，修正值
+		for(int i=n-2; i>=0; i--){
+			if(ratings[i] > ratings[i+1] && candys[i] <= candys[i+1]){
+				candys[i] = candys[i+1] + 1;
+			}
+		}
+		//糖果总数
+		int count = 0;
+		for(int i=0; i<n; i++){
+			count = count + candys[i];
+		}
+		
+		return count;
+    }
+}
