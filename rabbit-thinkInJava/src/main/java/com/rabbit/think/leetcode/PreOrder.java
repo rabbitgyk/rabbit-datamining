e929e2118e7144dbbfde79cb66d5a30e28ee477d
guoyankui <yankuiguo@gmail.com>
2014-09-09 23:31:38 +0800

    add traversal binary tree with stack in PreOrder

--- rabbit-thinkInJava/src/main/java/com/rabbit/think/leetcode/PreOrder.java
+++ rabbit-thinkInJava/src/main/java/com/rabbit/think/leetcode/PreOrder.java
@@ -2,6 +2,9 @@ package com.rabbit.think.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+
+import com.rabbit.think.leetcode.SumTreeNumbers.TreeNode;
 /**
  * Given a binary tree, return the preorder traversal of its nodes' values.
  * 
@@ -23,6 +26,28 @@ public class PreOrder {
 		vals.add(node.val);
 		traversal(vals, node.left);
 		traversal(vals, node.right);
+	}
+    
+    /**
+	 * 先序遍历二叉树，用栈
+	 * @param root
+	 */
+	private List<Integer> preTraversal(TreeNode root){
+		List<Integer> vals = new ArrayList<Integer>();
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		TreeNode p = root;
+		while(p != null || !stack.isEmpty()){
+			while(p != null){
+				vals.add(p.val);
+				stack.push(p);
+				p = p.left;
+			}
+			if(!stack.isEmpty()){
+				p = stack.pop();
+				p = p.right;
+			}
+		}
+		return vals;
 	}
 	
 	public class TreeNode {
