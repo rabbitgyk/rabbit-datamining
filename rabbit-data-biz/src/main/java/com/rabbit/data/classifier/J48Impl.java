67295fd2cc91f228dc04dcda630161df27117b7e
guoyankui <yankuiguo@gmail.com>
2014-09-09 13:00:17 +0800

    add J48 classifier

---
+++ rabbit-data-biz/src/main/java/com/rabbit/data/classifier/J48Impl.java
@@ -0,0 +1,85 @@
+package com.rabbit.data.classifier;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class J48Impl implements ClassifierI{
+
+	private Instances instances;
+	private J48 classifier;
+	private Evaluation eval;
+	
+	public J48Impl(Instances data) throws Exception{
+		instances = data;
+		classifier = new J48();
+	}
+	
+	@Override
+	public String runClassifier() throws Exception {
+		eval = new Evaluation(instances);
+		eval.crossValidateModel(classifier, instances, 10, new Random(1));
+		return eval.toSummaryString("\nClassify Results:\n", false);
+	}
+
+	@Override
+	public double pctCorrect() throws Exception {
+		if(eval != null)
+			return eval.pctCorrect();
+		else
+			throw new Exception("evaluation 未初始化.");
+	}
+
+	@Override
+	public double pctIncorrect() throws Exception {
+		if(eval != null)
+			return eval.pctIncorrect();
+		else
+			throw new Exception("evaluation 未初始化.");
+	}
+
+	@Override
+	public int numCorrect() throws Exception {
+		if(eval != null)
+			return (int) eval.correct();
+		else
+			throw new Exception("evaluation 未初始化.");
+	}
+
+	@Override
+	public int numIncorrect() throws Exception {
+		if(eval != null)
+			return (int) eval.incorrect();
+		else
+			throw new Exception("evaluation 未初始化.");
+	}
+
+	@Override
+	public void buildClassifier() throws Exception {
+		classifier.buildClassifier(instances);
+	}
+
+	@Override
+	public String classifyInstance(Instance ins) throws Exception {
+		double index = classifier.classifyInstance(ins);
+		return instances.classAttribute().value((int)index);
+	}
+
+	@Override
+	public Map<String, Double> distributionForInstance(Instance ins) throws Exception {
+		int numClass = instances.numClasses();
+		Map<String, Double> map = new HashMap<String, Double>();
+		double[] disClass = classifier.distributionForInstance(ins);
+		for(int i=0; i<numClass; i++){
+			String clazz = instances.classAttribute().value(i);
+			map.put(clazz, disClass[i]);
+		}
+		return map;
+	}
+
+}
--- rabbit-data-biz/src/test/java/com/rabbit/data/test/TestClassifier.java
+++ rabbit-data-biz/src/test/java/com/rabbit/data/test/TestClassifier.java
@@ -8,6 +8,7 @@ import weka.core.Instances;
 
 import com.rabbit.data.attributeSelection.ConnectSubGraphImpl;
 import com.rabbit.data.attributeSelection.GreedyStepwiseImpl;
+import com.rabbit.data.classifier.J48Impl;
 import com.rabbit.data.classifier.NaiveBayesImpl;
 
 public class TestClassifier {
@@ -16,25 +17,44 @@ public class TestClassifier {
 		FileReader frData = new FileReader("/home/rabbit/data/my/CountryFlag28.arff");
 		Instances data = new Instances(frData);
 		data.setClassIndex(data.numAttributes() - 1);
+		
 		NaiveBayesImpl nbi = new NaiveBayesImpl(data);
 		String summary = nbi.runClassifier();
-		System.out.println(summary);
+		System.out.println("NaiveBayesImpl:" + summary);
+		
+		J48Impl j48i = new J48Impl(data);
+		String summaryJ = j48i.runClassifier();
+		System.out.println("J48Impl:" + summaryJ);
+		
+		//-----------------------------------------------------------
 		
 		ConnectSubGraphImpl csgi = new ConnectSubGraphImpl(data);
 		csgi.buildAS();
 		Instances newData1 = csgi.endInstances();
 		System.out.println("ConnectSubGraphImpl attrs:"+newData1.numAttributes());
+		
 		NaiveBayesImpl nbi1 = new NaiveBayesImpl(newData1);
 		String summary1 = nbi1.runClassifier();
-		System.out.println(summary1);
+		System.out.println("NaiveBayesImpl:" + summary1);
+		
+		J48Impl j48i1 = new J48Impl(newData1);
+		String summaryJ1 = j48i1.runClassifier();
+		System.out.println("J48Impl:" + summaryJ1);
+		
+		//-----------------------------------------------------------
 		
 		GreedyStepwiseImpl gsi = new GreedyStepwiseImpl(data);
 		gsi.buildAS();
 		Instances newData2 = gsi.endInstances();
 		System.out.println("GreedyStepwiseImpl attrs:"+newData2.numAttributes());
+		
 		NaiveBayesImpl nbi2 = new NaiveBayesImpl(newData2);
 		String summary2 = nbi2.runClassifier();
-		System.out.println(summary2);
+		System.out.println("NaiveBayesImpl:" + summary2);
+		
+		J48Impl j48i2 = new J48Impl(newData2);
+		String summaryJ2 = j48i2.runClassifier();
+		System.out.println("J48Impl:" + summaryJ2);
 	}
 
 }
