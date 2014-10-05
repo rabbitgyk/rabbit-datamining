129b9c5af1d7784a56f5586336446b56ce41500e
guoyankui <yankuiguo@gmail.com>
2014-10-05 16:02:36 +0800

    comment the preorder stack

--- rabbit-thinkInJava/src/main/java/com/rabbit/think/leetcode/PreOrder.java
+++ rabbit-thinkInJava/src/main/java/com/rabbit/think/leetcode/PreOrder.java
@@ -2,9 +2,8 @@ package com.rabbit.think.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
+//import java.util.Stack;
 
-import com.rabbit.think.leetcode.SumTreeNumbers.TreeNode;
 /**
  * Given a binary tree, return the preorder traversal of its nodes' values.
  * 
@@ -28,29 +27,29 @@ public class PreOrder {
 		traversal(vals, node.right);
 	}
     
-    /**
-	 * 先序遍历二叉树，用栈
-	 * @param root
-	 */
-	private List<Integer> preTraversal(TreeNode root){
-		List<Integer> vals = new ArrayList<Integer>();
-        if(root == null){
-            return vals;
-        }
-		Stack<TreeNode> stack = new Stack<TreeNode>();
-		stack.push(root);
-		while(!stack.isEmpty()){
-			TreeNode node = stack.pop();
-			vals.add(node.val);
-			if(node.right != null){
-			    stack.push(node.right);
-			}
-			if(node.left != null){
-			    stack.push(node.left);
-			}
-		}
-		return vals;
-	}
+//    /**
+//	 * 先序遍历二叉树，用栈
+//	 * @param root
+//	 */
+//	private List<Integer> preTraversal(TreeNode root){
+//		List<Integer> vals = new ArrayList<Integer>();
+//        if(root == null){
+//            return vals;
+//        }
+//		Stack<TreeNode> stack = new Stack<TreeNode>();
+//		stack.push(root);
+//		while(!stack.isEmpty()){
+//			TreeNode node = stack.pop();
+//			vals.add(node.val);
+//			if(node.right != null){
+//			    stack.push(node.right);
+//			}
+//			if(node.left != null){
+//			    stack.push(node.left);
+//			}
+//		}
+//		return vals;
+//	}
 	
 	public class TreeNode {
 	     int val;
